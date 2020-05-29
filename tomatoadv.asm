cd4eb63de2e10aaf3e0b43fe214a65e38ac30c42
Unknown W. Brackets <checkins@unknownbrackets.org>
2020-05-28 19:52:13 -0700

    Add variable width font from anon.

--- tomatoadv.asm
+++ tomatoadv.asm
@@ -5,6 +5,11 @@ org $8078038; db $0B0         // move menu option text left 1 block
 org $8490470; dd $87F0000    // repoint item option text block pointer
 org $8490474; db $07          // make each item option text 7 letters long
 
-// make a blank space in the fonts, comment out to make the little tsu return in the Japanese font
+// make a blank space in the fonts
 org $8649636; db $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
 org $864453A; db $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+
+// insert two fonts (8x8, 8x16) with fixed punctuation
+// insert their width tables
+org $8642748
+incbin fontdata.bin
