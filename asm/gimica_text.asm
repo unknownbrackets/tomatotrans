b4e9383464ea4abdc59987eb3601d3c5adb3c14d
Unknown W. Brackets <checkins@unknownbrackets.org>
2021-06-22 22:59:04 -0700

    Center Gimica card descs in deck menu.

--- asm/gimica_text.asm
+++ asm/gimica_text.asm
@@ -14,6 +14,9 @@ b 0x08074D0A
 ; Tell drawing to center the card descriptions.
 .org 0x084906A7
 db 0x01
+; The re-arrange card descriptions too.
+.org 0x0849067F
+db 0x01
 ; Also the Gimica hub place description.
 .org 0x08490627
 db 0x01
