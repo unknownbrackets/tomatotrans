e48e2e83c5a776589e8f280ba0a19996bfdc3b1b
Unknown W. Brackets <checkins@unknownbrackets.org>
2021-07-04 09:54:22 -0700

    Allow place names to be wider.
    Only on the overworld map, still constrained inside the menu and saves.

--- tomatoadv.asm
+++ tomatoadv.asm
@@ -54,6 +54,9 @@
 ; Adjust the buttons on the title screen.
 .include "asm/title_screen.asm"
 
+; Adjust the place names on the overworld map.
+.include "asm/map_text.asm"
+
 ; Adjust buttons and replace text drawing routine.
 .include "asm/credits.asm"
 
